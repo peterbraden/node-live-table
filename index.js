22d79f121cdc6cf03636d4f1a9df2e594a9768fc
Peter Braden <peterbraden@peterbraden.co.uk>
2013-10-10 14:27:48 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var charm = require('charm')();
+charm.reset();
+var Table = require('cli-table')
+
+
+Table.prototype.render = function(){
+  var str = this.toString().split('\n')
+  for (var i = 0; i< str.length; i++){
+    for (var j = 0; j < str[i].length; j++){
+      charm.position(1+j, 1+i).write(str[i][j])
+    }
+  }
+}
+
+
+Table.prototype.pipe = function(){
+  charm.pipe.apply(charm, arguments);
+}
+
+
+Table.prototype.update = function(x, y, val){
+  this[x][y] = val
+  this.render()
+}
+
+modeule.exports = Table
+
+
+
